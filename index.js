37644dda0d00fd09e15aa03b0865f4f8c8e5ca49
Collin Argo <collinargo@gmail.com>
2019-02-18 15:10:31 -0500

    use apollo server as middleware

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema')
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
 
-server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`)
+server.applyMiddleware({ app })
+
+app.listen({ port }, () => {
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    console.log(`🚀 app ready at http://localhost:4000/`)
 })
