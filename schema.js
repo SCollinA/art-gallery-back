c6c542e00acb1e01ff19b45bf0261ad1a7a70bdc
Collin Argo <collinargo@gmail.com>
2019-02-18 15:02:25 -0500

    move schema to separate file

---
+++ schema.js
@@ -0,0 +1,109 @@
+const { gql } = require('apollo-server')
+
+// The GraphQL schema
+const typeDefs = gql`
+    input ArtworkInput {
+        id: ID
+        galleryId: ID
+        title: String 
+        width: Int
+        height: Int
+        medium: String
+        image: String
+        price: Float
+        sold: Boolean
+    }
+    
+    input GalleryInput {
+        id: ID
+        name: String
+        artworkIds: [ID]
+    }
+
+    type Query {
+        "get a collection of artworks"
+        getGallery(id: ID!): Gallery!
+        getAllGalleries: [Gallery]
+        "get a single artwork"
+        getArtwork(id: ID!): Artwork!
+        getAllArtworks: [Artwork]
+    }
+
+    type Mutation {
+        addGallery(input: GalleryInput!): Gallery!
+        updateGallery(id: ID!, input: GalleryInput!): Gallery!
+        deleteGallery(id: ID!): Boolean
+        addArtwork(input: ArtworkInput!): Artwork!
+        updateArtwork(id: ID!, input: ArtworkInput!): Artwork!
+        deleteArtwork(id: ID!): Boolean
+    }
+
+    type Gallery {
+        id: ID!
+        name: String
+    }
+
+    type Artwork {
+        id: ID!
+        galleryId: ID
+        title: String 
+        width: Int
+        height: Int
+        medium: String
+        image: String
+        price: Float
+        sold: Boolean
+    }
+`
+
+// A map of functions which return data for the schema.
+const resolvers = {
+    // get
+    Query: {
+        getGallery: (obj, args, context, info) => {
+            return Gallery.findOne({ where: { id: args.id } })
+        },
+        getAllGalleries: (obj, args, context, info) => {
+            return Gallery.findAll()
+        },
+        getArtwork: (obj, args, context, info) => {
+            return Artwork.findOne({ where: { id: args.id } })
+        },
+        getAllArtworks: (obj, args, context, info) => {
+            return Artwork.findAll()
+        }
+    },
+    // set
+    Mutation: {
+        addGallery: (obj, args, context, info) => {
+            return Gallery.create({...args.input})
+        },
+        updateGallery: (obj, args, context, info) => {
+            return Gallery.update({...args.input}, { 
+                where: { id: args.id },
+            })
+            .then(() => Gallery.findById(args.id))
+        },
+        deleteGallery: (obj, args, context, info) => {
+            return Gallery.destroy({ where: { id: args.id } })
+        },
+        addArtwork: (obj, args, context, info) => {
+            return Artwork.create({...args.input})
+        },
+        updateArtwork: (obj, args, context, info) => {
+            return Artwork.update({...args.input}, { 
+                where: { id: args.id },
+            })
+            .then(() => Artwork.findById(args.id))
+        },
+        deleteArtwork: (obj, args, context, info) => {
+            return Artwork.destroy({ where: { id: args.id } })
+        }, 
+    }
+    // subscribe
+}
+
+module.exports = {
+    typeDefs,
+    resolvers,
+}
