cbf3ccdc78eb1b93a74b8caea4b27c10d82f738f
Collin Argo <collinargo@gmail.com>
2019-02-18 10:38:57 -0500

    add gallery table

--- index.js
+++ index.js
@@ -4,24 +4,51 @@ const sequelize = new Sequelize('postgres:///apollo-server')
 
 // The GraphQL schema
 const typeDefs = gql`
-  type Query {
-    "A simple type for getting started!"
-    hello: String
-  }
-`;
+    type Query {
+        "A simple type for getting started!"
+        hello: String
+    }
+
+    type Mutation {
+        "update artwork"
+        updateArtwork: String
+    }
+
+    type Gallery {
+        artworks: [Artwork!]!
+    }
+
+    type Artwork {
+        id: ID
+        title: String
+        dimensions: {
+            width: Int
+            height: Int
+        }
+        medium: String
+        image: 
+        sold: Boolean
+    }
+`
 
 // A map of functions which return data for the schema.
 const resolvers = {
-  Query: {
-    hello: () => 'world'
-  }
-};
+    // get
+    Query: {
+        hello: () => 'world'
+    },
+    // set
+    Mutation: {
+        updateArtwork: () => 'update artwork'
+    }
+    // subscribe
+}
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+    typeDefs,
+    resolvers,
+})
 
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`)
-  });
+})
--- schema.sql
+++ schema.sql
@@ -1,8 +1,15 @@
+create table gallery (
+    id serial primary key,
+    name varchar(100)
+);
+
 create table artworks (
     id serial primary key,
+    gallery_id integer references gallery (id),
     title varchar(100),
     width integer,
     height integer,
     medium varchar(200),
-    image bytea
+    image bytea,
+    sold boolean
 );
