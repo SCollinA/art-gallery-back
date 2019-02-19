bd5607de4c23e80b6e1b5d039b842614fe0b9713
Collin Argo <collinargo@gmail.com>
2019-02-18 19:29:09 -0500

    fix db name

--- Sequelize.js
+++ Sequelize.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postgres:///apollo-server', {
+const sequelize = new Sequelize('postgres:///art-gallery', {
   operatorsAliases: Sequelize.Op
 })
 
 sequelize
 .authenticate()
 .then(() => {
-  console.log('Connection has been established successfully.')
-  sequelize.sync()
+    console.log('Connection has been established successfully.')
+    sequelize.sync({ force: true })
 })
 .catch(err => {
   console.error('Unable to connect to the database:', err)
