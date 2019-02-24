445709daa6cc21d9aeed41ea705ccdf4d449b3fc
Collin Argo <collinargo@gmail.com>
2019-02-24 09:10:43 -0500

    add cors origin to Apollo Server init

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const { typeDefs, resolvers } = require('./schema')
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ authorization: req.get('Authorization') })
+    context: ({ req }) => ({ authorization: req.get('Authorization') }),
+    cors: {
+        origin: 'http://localhost:8000',
+    }
 })
 
 server.listen().then(({ url }) => console.log(`🚀 Server ready at ${url}`))
