1418dcc8aea549db8fd1d36cd2380649477b8cf6
Collin Argo <collinargo@gmail.com>
2019-03-16 09:50:52 -0400

    install ssl support

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const { ApolloServer } = require('apollo-server');
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
 const { typeDefs, resolvers } = require('./schema')
+import fs from 'fs'
+import https from 'https'
+import http from 'http'
 
-const server = new ApolloServer({
+const configurations = {
+    // Note: You may need sudo to run on port 443
+    production: { ssl: true, port: 443, hostname: 'example.com' },
+    development: { ssl: false, port: 4000, hostname: 'localhost' }
+  }
+  
+const environment = process.env.NODE_ENV || 'production'
+const config = configurations[environment]
+
+const apollo = new ApolloServer({
     typeDefs,
     resolvers,
+    
+    // code below would be used to authenticate subscription
+    // subscriptions: {
+    //     onConnect: (connectionParams, webSocket) => {
+    //         if (connectionParams.authToken) {
+    //             return checkLoggedIn(connectionParams.authToken)
+    //         }
+    //         throw new Error('Missing auth token')
+    //     }
+    // },
     context: ({ req, connection }) => {
         if (connection) {
             return connection.context
         } else {
             const authorization = req.headers.authorization || ''
-            console.log(authorization)
             return { authorization }
         }
         // ({ authorization: req.get('Authorization') })
@@ -23,6 +45,28 @@ const server = new ApolloServer({
         ],
     }
 })
+
+const app = express()
+apollo.applyMiddleware({ app })
+
+// Create the HTTPS or HTTP server, per configuration
+var server
+if (config.ssl) {
+  // Assumes certificates are in .ssl folder from package root. Make sure the files
+  // are secured.
+  server = https.createServer(
+    {
+      key: fs.readFileSync(`./ssl/${environment}/server.key`),
+      cert: fs.readFileSync(`./ssl/${environment}/server.crt`)
+    },
+    app
+  )
+} else {
+  server = http.createServer(app)
+}
+
+// Add subscription support
+apollo.installSubscriptionHandlers(server)
 
 server.listen({ 
     port: 4000, 
